8fda1b3d021b18d4ba08c495f9b00003ddba4a64
somethinrother <jamesneilson4@gmail.com>
2018-01-08 14:12:37 -0500

    User can log in and out with proper cookie creation, edits to application layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to projects_url, notice: "Logged in!"
+      redirect_to books_url, notice: "Logged in!"
     else
       flash.now[:notice] = "Username or password is not correct."
       render "new"
