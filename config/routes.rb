45769a620c1128deb005085a602d66ac320eeffe
somethinrother <jamesneilson4@gmail.com>
2018-01-08 13:56:16 -0500

    Create sessions controller, add methods and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'login' => 'user_sessions#new', :as => :login
+
+  delete 'logout' => 'user_sessions#destroy', :as => :logout
+
   resources :reading_lists
   resources :genres, only: [:index, :show]
   resources :users
   resources :books, only: [:index, :show, :edit, :update] do
     resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   end
+  resources :sessions, only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
