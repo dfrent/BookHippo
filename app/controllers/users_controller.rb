decaf03a5ae6477c897fe5753111a610eb508ed9
KYLE LIPTON <lipton.kyle@gmail.com>
2018-01-08 14:43:22 -0500

    user signup page created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,25 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new
+
+    @user.email = params[:user][:email]
+    @user.username = params[:user][:username]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      # # Auto-login on succesful signup
+      flash[:notice] = 'Account successfully created!'
+      session[:user_id] = @user.id
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def edit
