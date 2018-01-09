502fcc44f7558a25e21507e41fbf942f5ff732c7
cordell <cordell.perry@gmail.com>
2018-01-08 23:14:14 -0500

    migrated columns in books table

--- db/migrate/20180109035554_adding_books_columns.rb
+++ db/migrate/20180109035554_adding_books_columns.rb
@@ -1,4 +1,8 @@
 class AddingBooksColumns < ActiveRecord::Migration[5.1]
   def change
+    remove_column :books, :number_of_pages
+    add_column :books, :isbn, :integer
+    add_column :books, :page_count, :integer
+    add_column :books, :country_of_origin, :string
   end
 end
