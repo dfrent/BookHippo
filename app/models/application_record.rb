2dc10c4988eb5987fad15734fa8bbb2b75b10825
cordell <cordell.perry@gmail.com>
2018-01-09 15:14:12 -0500

    search bar logic

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+
+  def self.find_user(search)
+    where(" username LIKE ? OR email LIKE ? " , "%#{search}%", "%#{search}%")
+  end
+
+  def self.look_for(search)
+    where(" isbn LIKE ? OR username LIKE ? OR title LIKE ? OR author LIKE ? OR email LIKE ? ", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
+
+  def search_for
+
+  end
+
 end
