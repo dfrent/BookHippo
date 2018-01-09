54d54f2388525e3e7490f3eaf9d27be236d0301b
somethinrother <jamesneilson4@gmail.com>
2018-01-08 21:22:46 -0500

    Add data associations to user and relationship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :books, through: :genres
   has_many :books, through: :reading_lists
 
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
   has_and_belongs_to_many :genres
   has_many :reviews
   has_many :reading_lists
