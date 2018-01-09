e7f570c2e634d61a48265b08ac9a2c34442b2ca5
Eric Marrone <emarrone17@gmail.com>
2018-01-09 16:45:06 -0500

    Add creation of isbn in find or call api method in books model. Add uniqueness validation for isbn in books

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,16 +6,19 @@ class Book < ApplicationRecord
   has_many :reviews
   has_many :reading_lists
 
+  validates :isbn, uniqueness: true
+
   def self.find_or_api_call(isbn)
     book = Book.find_by(isbn: isbn)
     if book
       return book
     else
       book_response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn=#{isbn}&key=#{ENV['GBOOKS_KEY']}")
-      book = book_response.parsed_response["items"][0]["volumeInfo"]
-      Book.create(title: book["title"], author: book["authors"][0], description: book["description"], book_cover: book["imageLinks"]["thumbnail"])
-    end
 
+      book = book_response.parsed_response["items"][0]["volumeInfo"]
+
+      Book.create(isbn: isbn, title: book["title"], author: book["authors"][0], description: book["description"], book_cover: book["imageLinks"]["thumbnail"], genre_id: 1)
+    end
   end
 
 
