9a94b5dd0069f411d51cc84e3ef661f9baca2e52
somethinrother <jamesneilson4@gmail.com>
2018-01-09 15:41:54 -0500

    add @users to new user controller methods, and create show_follow view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,12 +63,14 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user  = User.find(params[:id])
+    @users = @user.following
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
+    @users = @user.followers
     render 'show_follow'
   end
 
