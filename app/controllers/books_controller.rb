ab5f9d9c936e25089ca4275373aa9336f21a0436
somethinrother <jamesneilson4@gmail.com>
2018-01-09 11:41:55 -0500

    Minor edits to book index API call syntax

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,17 @@ class BooksController < ApplicationController
   def index
     response = HTTParty.get("https://api.nytimes.com/svc/books/v3/lists.json?api-key=#{ENV['NYTIMES_KEY']}&list=mass-market-paperback")
 
-
     @books = []
 
     response.parsed_response["results"].each do |result|
 
-      @isbn = result["book_details"][0]["primary_isbn10"]
+      isbn = result["book_details"][0]["primary_isbn10"]
 
+      goog_response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn=#{isbn}&key=#{ENV['GBOOKS_KEY']}")
 
-      goog_response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn=#{@isbn}&key=#{ENV['GBOOKS_KEY']}")
+      book_img = goog_response.parsed_response["items"][0]["volumeInfo"]["imageLinks"]["smallThumbnail"]
 
-      bookimg = goog_response.parsed_response["items"][0]["volumeInfo"]["imageLinks"]["smallThumbnail"]
-
-
-      @books << { title: result["book_details"][0]["title"], author: result["book_details"][0]["author"], description: result["book_details"][0]["description"], isbn: result["book_details"][0]["primary_isbn10"], bookimage: bookimg }
+      @books << { title: result["book_details"][0]["title"], author: result["book_details"][0]["author"], description: result["book_details"][0]["description"], isbn: result["book_details"][0]["primary_isbn10"], book_image: book_img }
     end
   end
 
