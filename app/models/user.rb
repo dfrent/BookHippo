85558aa0e931b33f9839146309d0066af939432f
somethinrother <jamesneilson4@gmail.com>
2018-01-08 21:28:44 -0500

    Add custom methods to the user model and following associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,24 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   has_and_belongs_to_many :genres
   has_many :reviews
   has_many :reading_lists
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
