617d33264d79ef33b8523cdcae24d337be5d3c81
somethinrother <jamesneilson4@gmail.com>
2018-01-09 15:14:42 -0500

    Add relationship routes, and _follow and _unfollow partials

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :sessions, only: [:create]
+  resources :relationships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
