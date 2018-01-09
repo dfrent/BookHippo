72dc0650718438e723ddacc1222c7a21273af3e6
Eric Marrone <emarrone17@gmail.com>
2018-01-09 15:28:29 -0500

    Add new migration for new book columns. Run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109035554) do
+ActiveRecord::Schema.define(version: 20180109201547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20180109035554) do
     t.integer "isbn"
     t.integer "page_count"
     t.string "country_of_origin"
+    t.text "description"
+    t.string "small_thumbnail"
   end
 
   create_table "genres", force: :cascade do |t|
