6f29da8cacbd3ce606d58255c24193031f4be243
somethinrother <jamesneilson4@gmail.com>
2018-01-10 18:00:33 -0500

    Make buttons disappear on click

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+document.addEventListener("DOMContentLoaded", function(e){
+  read_buttons = document.querySelectorAll('.read-btn')
+
+  read_buttons.forEach(function(button){
+    button.addEventListener("click", function(e){
+      e.target.style.visibility = 'hidden';
+    });
+  });
+});
