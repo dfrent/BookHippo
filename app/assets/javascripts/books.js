7b88dcbc4765566799526b12451a07ea92fb0049
somethinrother <jamesneilson4@gmail.com>
2018-01-11 12:21:31 -0500

    All buttons hide if user clicks a higher level of read status

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -15,8 +15,11 @@ document.addEventListener("DOMContentLoaded", function(e){
         reading_message.innerText = "You currently have '" + book_title + "' in your 'want to read' list"
       } else if (e.target.value === "Currently Reading") {
         reading_message.innerText = "You are currently reading '" + book_title + "'. Enjoy it!"
+        read_buttons[0].style.visibility = 'hidden';
       } else if (e.target.value === "Finished Reading") {
         reading_message.innerText = "You finished reading '" + book_title + "'. Awesome!!"
+        read_buttons[0].style.visibility = 'hidden';
+        read_buttons[1].style.visibility = 'hidden';
       }
     });
   });
