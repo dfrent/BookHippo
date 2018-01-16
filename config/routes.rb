297cf2c7ef754533e1c9b8a70867bc63c9f1907a
somethinrother <jamesneilson4@gmail.com>
2018-01-16 15:25:27 -0500

    Add routes for conversation and nested routes for messages

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :relationships, only: [:create, :destroy]
 
+  resources :conversations do
+    resources :messages
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
