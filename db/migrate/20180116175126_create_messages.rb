35ff60e559365801651406bd519f3446231a9a24
somethinrother <jamesneilson4@gmail.com>
2018-01-16 12:57:14 -0500

    Redo migrations for Conversation and Message model

--- db/migrate/20180116175126_create_messages.rb
+++ db/migrate/20180116175126_create_messages.rb
@@ -5,7 +5,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
       t.references :conversation, index: true
       t.references :user, index: true
       t.boolean :read, :default => false
-      
+
       t.timestamps
     end
   end
