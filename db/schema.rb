b7655a367b4512d119575a5a080071c2b5c3e7d1
KYLE LIPTON <lipton.kyle@gmail.com>
2018-01-16 20:17:59 -0500

    no changes actually made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180112230936) do
+ActiveRecord::Schema.define(version: 20180115063655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 20180112230936) do
   create_table "interests", force: :cascade do |t|
     t.integer "user_id"
     t.integer "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
