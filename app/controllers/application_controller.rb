ee2d09d75cdb159b761081efe8c2040e62f0c210
somethinrother <jamesneilson4@gmail.com>
2018-01-18 18:08:14 -0500

    Set time to EST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  config.time_zone = 'Eastern Time (US & Canada)'
+
+  around_action :set_time_zone
+
   helper_method :current_user, :logged_in?
 
   def current_user
@@ -25,6 +29,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
-
+  def set_time_zone
+    Time.use_zone("EST") { yield }
+  end
 end
