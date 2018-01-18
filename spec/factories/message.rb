2c42070c304d417faab12b04f113f21058e0857f
somethinrother <jamesneilson4@gmail.com>
2018-01-18 17:55:44 -0500

    Write tests for message model

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :message do
     body "Hello world!"
+    conversation
   end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -2,13 +2,68 @@ require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
   def test_message_saves_when_body_present
-    conversation = build(:conversation)
-    conversation.save
     message = build(:message)
-    message.conversation = conversation
-    message.user = conversation.sender
+    message.user = build(:user)
     message.save
-    
+
     assert message.persisted?
   end
+
+  def test_message_does_not_save_without_user
+    message = build(:message)
+    message.save
+
+    refute message.persisted?
+  end
+
+  def test_message_does_not_save_without_conversation
+    message = build(:message)
+    message.conversation = nil
+    message.save
+
+    refute message.persisted?
+  end
+
+  def test_message_does_not_save_without_body
+    message = build(:message)
+    message.body = nil
+    message.save
+
+    refute message.persisted?
+  end
+
+  def test_message_is_not_read_when_saved
+    message = build(:message)
+    message.user = build(:user)
+    message.save
+
+    actual = message.read
+    expected = false
+
+    assert_equal(actual, expected)
+  end
+
+  def test_message_is_read_after_read_message_method_is_called
+    message = build(:message)
+    message.user = build(:user)
+    message.save
+    Message.read_message(message)
+
+    actual = message.read
+    expected = true
+
+    assert_equal(actual, expected)
+  end
+
+  # def test_message_time_displays_time_properly
+  #   Timecop.freeze(Time.now) do
+  #     message = build(:message)
+  #     message.user = build(:user)
+  #     message.save
+  #
+  #     actual = message.message_time
+  #     expected = Time.now.strftime("%m/%d/%y at %l:%M %p")
+  #     assert_equal(actual, expected)
+  #   end
+  # end
 end
