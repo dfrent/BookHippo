dcf81af22cb09ee5f4b1ca3e4120d28bcaf52294
Eric Marrone <emarrone17@gmail.com>
2018-01-26 14:16:28 -0500

    Action cable multi room chat now working.

--- app/channels/chats_channel.rb
+++ app/channels/chats_channel.rb
@@ -9,6 +9,8 @@ class ChatsChannel < ApplicationCable::Channel
     case data["do"]
     when "join-channel"
       join_channel(data)
+    when "msg"
+      msg(data)
     else
       transmit({msg:"I don't understand", action: data["do"]})
     end
@@ -18,5 +20,12 @@ class ChatsChannel < ApplicationCable::Channel
     channel = data["href"].split('/')[-2..-1].join('-')
     transmit({do: "msg", content: "I am connecting you to #{channel}"})
     stream_from channel
+  end
+
+  def msg(data)
+    username = User.find(data["cookie"].split('=')[1]).username
+    channel = data["href"].split('/')[-2..-1].join('-')
+    data["username"] = username
+    ActionCable.server.broadcast channel, data
   end
 end
