838245462637aa8412bcb0db1bbdf3ed86157d06
somethinrother <jamesneilson4@gmail.com>
2018-01-29 12:11:39 -0500

    Drop book_clubs_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129153003) do
+ActiveRecord::Schema.define(version: 20180129170827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,13 +23,6 @@ ActiveRecord::Schema.define(version: 20180129153003) do
     t.text "goal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "book_clubs_users", id: false, force: :cascade do |t|
-    t.bigint "book_club_id", null: false
-    t.bigint "user_id", null: false
-    t.index ["book_club_id"], name: "index_book_clubs_users_on_book_club_id"
-    t.index ["user_id"], name: "index_book_clubs_users_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
