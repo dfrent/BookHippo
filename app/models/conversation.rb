5156b911d18d388be9d4c0ad0659ca2e16833392
James Neilson <jamesneilson@Jamess-MacBook-Pro.local>
2018-03-11 22:01:42 -0400

    Correct offenses for Layout/SpaceInLambdaLiteral

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,7 +6,7 @@ class Conversation < ApplicationRecord
 
   validate :sender_is_not_recipient
 
-  scope :between, -> (sender_id,recipient_id) do
+  scope :between, ->(sender_id,recipient_id) do
     where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
   end
 
