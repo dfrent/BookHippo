1c0878dec2085ebc60fe17ed77902a456dd305ea
James Neilson <jamesneilson@Jamess-MacBook-Pro.local>
2018-03-15 00:33:12 -0400

    Split apart self.find_or_api_call to reduce complexity

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,24 +15,42 @@ class Book < ApplicationRecord
   validates :isbn, uniqueness: true
   validates :isbn, :author, :title, :book_cover, :description, presence: true
 
-  def self.find_or_api_call(isbn)
+  def self.book_selector(isbn)
     book = Book.find_by(isbn: isbn)
-    if book
-      return book
-    else
-      book_response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn=#{isbn}&key=#{ENV['GBOOKS_KEY']}")
+    return book if book
+  end
 
-      book = book_response.parsed_response["items"][0]["volumeInfo"]
-      authors = book["authors"]
-      if authors
-        authors_string = authors.join(", ")
-      end
-      google_id = book_response.parsed_response["items"][0]["id"]
-      if book
-        Book.create(isbn: isbn, title: book["title"], author: authors_string, description: book["description"], book_cover: book["imageLinks"]["thumbnail"], small_thumbnail: book["imageLinks"]["smallThumbnail"], genre_id: 20, google_id: google_id, page_count: book["pageCount"], average_rating: book["averageRating"], published_date: book["publishedDate"], publisher: book["publisher"])
-      else
-        return "Book not found"
-      end
+  def self.api_call(isbn)
+    book_response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn=#{isbn}&key=#{ENV['GBOOKS_KEY']}")
+    book = book_response.parsed_response["items"][0]
+    return book if book_response
+  end
+
+  def self.new_book_data(isbn)
+    book = Book.api_call(isbn)
+    book_volume_info = book["volumeInfo"]
+    authors = book["authors"] if book
+    authors_string = authors.join(", ") if authors
+    google_id = book["id"]
+    book_data = { title: book_volume_info['title'],
+                  author: authors_string,
+                  description: book_volume_info['description'],
+                  genre_id: 20,
+                  google_id: google_id,
+                  page_count: book_volume_info["pageCount"],
+                  average_rating: book_volume_info["averageRating"],
+                  published_date: book_volume_info["publishedDate"],
+                  publisher: book_volume_info["publisher"] }
+    return book_data
+  end
+
+  def self.find_or_api_call(isbn)
+    book = Book.book_selector(isbn)
+    return book if !book.nil?
+    book = Book.new_book_data(isbn) if book.nil?
+    return book if !book.nil?
+    if book.nil?
+      return "Book not found"
     end
   end
 
