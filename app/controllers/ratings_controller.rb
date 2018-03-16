a71ce262288cd0442e14fb867eb2cee9e43a3204
James <jamesneilson@Jamess-MacBook-Pro.local>
2018-03-15 23:02:44 -0400

    Minor edits

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,22 +1,24 @@
 class RatingsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def user_rating_display
+    @rating = Rating.find_by(user_id: current_user.id, book_id: @book.id)
+
+    if @rating == nil
+      @rating = Rating.create(book_id: @book.id, user_id: current_user.id, stars: params[:rating])
+
+    else
+      @rating.stars = params[:rating]
+      @rating.save!
+    end
+  end
+
   def create
     @book = Book.find_by_isbn(params[:book_id])
     @rating = Rating.new
-
     if logged_in?
-      @rating = Rating.find_by(user_id: current_user.id, book_id: @book.id)
-
-      if @rating == nil
-        @rating = Rating.create(book_id: @book.id, user_id: current_user.id, stars: params[:rating])
-
-      else
-        @rating.stars = params[:rating]
-        @rating.save!
-      end
+      user_rating_display
     end
-
     Rails.logger.info(@rating.errors.inspect)
     if request.xhr?
       render json: @rating.stars
