33f96589c7d4387415f68121da530bc6e9b05a1a
James Neilson <james@quandl.com>
2019-03-17 03:59:29 -0400

    Remove logic from book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,36 +15,6 @@ class Book < ApplicationRecord
   validates :isbn, uniqueness: true
   validates :isbn, :author, :title, :book_cover, :description, presence: true
 
-  def self.new_book_data(isbn)
-    google = Tools::Google.new
-    book = google.book_from_isbn(isbn)
-    book_volume_info = book['volumeInfo']
-    authors = book['authors'] if book
-    authors_string = authors.join(', ') if authors
-    google_id = book['id']
-    book_data = { title: book_volume_info['title'],
-                  author: authors_string,
-                  description: book_volume_info['description'],
-                  genre_id: 20,
-                  google_id: google_id,
-                  page_count: book_volume_info['pageCount'],
-                  average_rating: book_volume_info['averageRating'],
-                  published_date: book_volume_info['publishedDate'],
-                  publisher: book_volume_info['publisher'] }
-    return book_data
-  end
-
-  def self.find_or_api_call(isbn)
-    book = Book.find_by(isbn: isbn)
-    return book if book
-    book = Book.new(Book.new_book_data(isbn))
-    book.save if book.valid?
-    return book if book.valid?
-    if book.nil?
-      return 'Book not found or invalid'
-    end
-  end
-
   def average_rating
     # First we check to see if the book in question has any ratings yet. If not, this returns 0 and a display will change in the book show view
     if !ratings.any?
