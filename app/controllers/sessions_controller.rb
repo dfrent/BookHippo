d8aecd94494c760637e12ec7ce8c1d28616ef876
James Neilson <james@quandl.com>
2019-03-17 17:11:10 -0400

    Update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
