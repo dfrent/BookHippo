180c9ee8a99815a3a8c219b0a6b049fd4f3c6fd7
James Neilson <james@quandl.com>
2019-03-17 19:38:45 -0400

    Refactor search_for_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   config.time_zone = 'Eastern Time (US & Canada)'
-  around_action :set_time_zone
   helper_method :current_user, :logged_in?
 
   def current_user
@@ -19,9 +18,5 @@ class ApplicationController < ActionController::Base
     return if current_user
     flash[:alert] = 'Please log in first.'
     redirect_to login_url
-  end
-
-  def set_time_zone
-    Time.use_zone('EST') { yield }
   end
 end
--- app/controllers/search_for_controller.rb
+++ app/controllers/search_for_controller.rb
@@ -1,23 +1,30 @@
 class SearchForController < ApplicationController
+  GOOGLE = Tools::Google.new
+
   def search_for
     redirect_to root_url unless params[:search]
     query = params[:search].downcase
-    google = Tools::Google.new
 
     @users = User.where(' username LIKE ? ', "%#{query}%")
-    @google_items = google.book_search(query)
-    return unless @google_items
+    google_items = GOOGLE.book_search(query)
+    return unless google_items
 
-    @books = @google_items[0..19].each_with_object([]) do |item, items|
-      isbn = google.isbn_10_from_api(item['volumeInfo']['industryIdentifiers'])
+    @books = validate_book_search(google_items)
+    render :index
+  end
+
+  private
+
+  def validate_book_search(books)
+    books[0..19].each_with_object([]) do |item, items|
+      isbn = GOOGLE.isbn_10_from_api(item['volumeInfo']['industryIdentifiers'])
       next unless isbn
 
-      google.isbn = isbn
-      book = google.find_or_api_call
+      GOOGLE.isbn = isbn
+      book = GOOGLE.find_or_api_call
       next if book.nil?
 
       items << book
     end
-    render :index
   end
 end
