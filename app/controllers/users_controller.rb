127bba3fe23dfb1baf4c55ec56277a080a6c0721
James Neilson <james@quandl.com>
2019-03-17 16:02:16 -0400

    Refactor users_to_follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_logged_in, except: [:new, :create]
+  before_action :ensure_logged_in, except: %i[new create]
 
   def index
   end
