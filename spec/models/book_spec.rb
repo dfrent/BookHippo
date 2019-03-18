d72d5eebb060ebcb54397b7ff4ba170ae49d1c0b
James Neilson <james@quandl.com>
2019-03-17 21:23:07 -0400

    More passing tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,12 +6,32 @@ RSpec.describe Book, :type => :model do
     expect(build(:book)).to be_valid
   end
 
-  it 'is not valid without an isbn'
-  it 'is not valid without an author'
-  it 'is not valid without a title'
-  it 'is not valid without a book cover'
-  it 'is not valid without a description'
-  it 'is not valid if isbn is taken'
+  it 'is not valid without an isbn' do
+    expect(build(:book, isbn: nil)).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    expect(build(:book, author: nil)).not_to be_valid
+  end
+
+  it 'is not valid without a title' do
+    expect(build(:book, title: nil)).not_to be_valid
+  end
+
+  it 'is not valid without a book cover' do
+    expect(build(:book, book_cover: nil)).not_to be_valid
+  end
+
+  it 'is not valid without a description' do
+    expect(build(:book, description: nil)).not_to be_valid
+  end
+
+  it 'is not valid if isbn is taken' do
+    book = create(:book)
+    duplicate_book = build(:book, isbn: book.isbn)
+    expect(duplicate_book).not_to be_valid
+  end
+
 
   # RELATIONSHIPS
   it 'is not valid without a genre'
