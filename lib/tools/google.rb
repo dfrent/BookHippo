f782977caeec92cbbc8942f98b7b2d49a1b5ad27
James Neilson <james@quandl.com>
2019-03-20 19:54:37 -0400

    Updating methods to make more sense

--- lib/tools/google.rb
+++ lib/tools/google.rb
@@ -9,28 +9,26 @@ module Tools
       @isbn = isbn
     end
 
-    def find_or_api_call
-      book = Book.find_by(isbn: @isbn)
-      return book if book
-      create_book
-    end
-
-    def update_books_by_genre
+    def self.update_books_by_genre
       books_for_all_genres.each do |item|
-        isbn_10_from_api(item['volumeInfo']['industryIdentifiers'])
-        next if @isbn.nil? || Book.exists?(@isbn)
+        isbn = isbn_10_from_api(item['volumeInfo']['industryIdentifiers'])
+        next if isbn.nil? || Book.exists?(isbn)
         create_book(genre.id)
       end
     end
 
-    def assign_images_to_all_books
+    def self.assign_images_to_all_books
       Book.all.map do |book|
         next if book.book_cover || book.small_thumbnail
-        @isbn = book.isbn
-        @book = book
-        prep_book_for_update
-        @book.save if @book.valid?
+        prep_book_for_update(book, book.isbn)
+        book.save if book.valid?
       end
+    end
+
+    def find_or_api_call
+      book = Book.find_by(isbn: @isbn)
+      return book if book
+      create_book
     end
 
     def book_search(query)
@@ -40,18 +38,16 @@ module Tools
 
     def isbn_10_from_api(identifiers)
       identifiers&.each do |identifier|
-        @isbn = identifier['identifier'] if identifier['type'] == 'ISBN_10'
+        return identifier['identifier'] if identifier['type'] == 'ISBN_10'
       end
-      @isbn
     end
 
     private
 
     # Lookup
 
-    def book_from_isbn
-      return unless @isbn
-      response = HTTParty.get("#{GOOGLE_ENDPOINT}isbn=#{@isbn}&key=#{GBOOKS_KEY}")
+    def book_from_isbn(isbn)
+      response = HTTParty.get("#{GOOGLE_ENDPOINT}isbn=#{isbn}&key=#{GBOOKS_KEY}")
       response.parsed_response['items'][0]
     end
 
@@ -70,7 +66,7 @@ module Tools
 
     def create_book(genre_id = 20)
       @book = Book.new
-      return unless prep_book_for_update
+      return unless prep_book_for_update(@book)
       @book.update_attributes(book_attributes(@volume_info, genre_id, @book))
       return @book if @book.valid?
       RejectedIsbn.record_invalid_isbn(@isbn)
@@ -99,9 +95,9 @@ module Tools
       authors.length > 1 ? authors : authors.join(', ')
     end
 
-    def prep_book_for_update(book)
-      return false if !@isbn || RejectedIsbn.isbn_is_rejected(@isbn)
-      book_data = book_from_isbn
+    def prep_book_for_update(book, isbn = nil)
+      return false if RejectedIsbn.isbn_is_rejected(isbn)
+      book_data = book_from_isbn(isbn)
       images = book_data['volumeInfo']['imageLinks']
       assign_images(images, book)
       book_data['volumeInfo']
