c167d7cf6461d74209aef8937a19445b62fca94e
James Neilson <james@quandl.com>
2019-03-20 20:37:32 -0400

    Genre update is working

--- lib/tools/google.rb
+++ lib/tools/google.rb
@@ -9,15 +9,35 @@ module Tools
       @isbn = isbn
     end
 
-    def self.update_books_by_genre
-      books_for_all_genres.each do |item|
-        isbn = isbn_10_from_api(item['volumeInfo']['industryIdentifiers'])
-        next if isbn.nil? || Book.exists?(isbn)
-        create_book(genre.id)
+    def update_books_by_genre
+      books_for_all_genres.each do |books_list|
+        books_list.each do |genre_id, books|
+          books.each do |book|
+            next unless book && book['volumeInfo']
+            isbns = book['volumeInfo']['industryIdentifiers']
+            next unless isbns
+            isbn = isbn_10_from_isbns(isbns)
+            next if isbn.nil? || Book.exists?(isbn)
+            create_book(genre_id, isbn)
+          end
+        end
       end
     end
 
-    def self.assign_images_to_all_books
+    def books_for_all_genres
+      Genre.all.map do |genre|
+        books_in_genre(genre.name, genre.id)
+      end
+    end
+
+    def books_in_genre(genre_name, genre_id)
+      response = HTTParty.get("#{GOOGLE_ENDPOINT}subject=#{genre_name}&key=#{GBOOKS_KEY}")
+      {
+        "#{genre_id}": response.parsed_response['items']
+      }
+    end
+
+    def assign_images_to_all_books
       Book.all.map do |book|
         next if book.book_cover || book.small_thumbnail
         prep_book_for_update(book, book.isbn)
@@ -25,24 +45,24 @@ module Tools
       end
     end
 
+    def isbn_10_from_isbns(identifiers)
+      isbn = nil
+      identifiers&.each do |identifier|
+        isbn = identifier['identifier'] if identifier['type'] == 'ISBN_10'
+      end
+      isbn
+    end
+
     def find_or_api_call
       book = Book.find_by(isbn: @isbn)
       return book if book
-      create_book
+      create_book(@isbn)
     end
 
     def book_search(query)
       response = HTTParty.get("#{GOOGLE_ENDPOINT}#{query}&key=#{GBOOKS_KEY}")
       response.parsed_response['items']
     end
-
-    def isbn_10_from_api(identifiers)
-      identifiers&.each do |identifier|
-        return identifier['identifier'] if identifier['type'] == 'ISBN_10'
-      end
-    end
-
-    private
 
     # Lookup
 
@@ -51,32 +71,25 @@ module Tools
       response.parsed_response['items'][0]
     end
 
-    def books_in_genre(genre_name)
-      response = HTTParty.get("#{GOOGLE_ENDPOINT}subject=#{genre_name}&key=#{GBOOKS_KEY}")
-      response.parsed_response['items']
-    end
-
-    def books_for_all_genres
-      Genre.all.map do |genre|
-        books_in_genre(genre.name)
-      end
-    end
-
     # Model Creation/ Updates
 
-    def create_book(genre_id = 20)
-      @book = Book.new
-      return unless prep_book_for_update(@book)
-      @book.update_attributes(book_attributes(@volume_info, genre_id, @book))
-      return @book if @book.valid?
-      RejectedIsbn.record_invalid_isbn(@isbn)
+    def create_book(genre_id = 20, isbn = nil)
+      book = Book.new
+      volume_info = prep_book_for_update(book)
+      return unless volume_info
+      book.assign_attributes(book_attributes(volume_info, genre_id, book))
+      if book.valid?
+        book.save
+        return book
+      end
+      RejectedIsbn.record_invalid_isbn(isbn)
       nil
     end
 
     def book_attributes(volume_info, genre_id, book)
       {
         title: volume_info['title'],
-        author: account_for_multiple_authors,
+        author: account_for_multiple_authors(volume_info),
         description: volume_info['description'] || '',
         genre_id: genre_id,
         isbn: isbn,
@@ -88,9 +101,9 @@ module Tools
       }
     end
 
-    def account_for_multiple_authors
-      return unless @volume_info['authors']
-      authors = @volume_info['authors']
+    def account_for_multiple_authors(volume_info)
+      return unless volume_info['authors']
+      authors = volume_info['authors']
       return '' unless authors
       authors.length > 1 ? authors : authors.join(', ')
     end
