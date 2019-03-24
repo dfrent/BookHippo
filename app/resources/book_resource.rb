8dc67079125238089965c40336bdf738376f9730
James Neilson <james@quandl.com>
2019-03-23 21:32:06 -0400

    Update resource to respond to isbn

--- app/resources/book_resource.rb
+++ app/resources/book_resource.rb
@@ -4,4 +4,6 @@ class BookResource < JSONAPI::Resource
   attributes :title, :author, :published_date, :average_rating, :book_cover, :genre_id, :isbn,
              :page_count, :country_of_origin, :description, :small_thumbnail, :ny_times_list,
              :google_id, :publisher
+
+  primary_key :isbn
 end
